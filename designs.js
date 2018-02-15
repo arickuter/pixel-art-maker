6f6b36fa42bcc80bac8986a05ccbe3ee2a8afcef
Aric Kuter <arickuter99@gmail.com>
2018-02-15 14:02:33 +0200

    Initial commit - full completed project

---
+++ designs.js
@@ -0,0 +1,49 @@
+//Function that creates the table, blank
+function makeGrid() {
+  const height = $('#inputHeight').val(); //Gets the value of the inputHeight field
+  const width = $('#inputWeight').val(); //Gets the value of the inputWeight field
+
+  const tbl = $('#pixelCanvas'); //Sets the tbl to the table object
+  for (let i=0; i < height; i++) {
+    let mRow = $('<tr id="' + i + '"">');
+
+    tbl.append(mRow); //Adds the row to the table
+    for (let j = 0; j < width; j++) {
+      mRow.append($('<td id="' + j + '"">')); //Populates the row with cells
+    }
+  }
+  $('td').trigger('click'); //Triggers the click event for the <td> elements. If you don't there is a weird bug, this overcame it
+}
+
+//Function that converts the rgb color format to hex format
+function rgb2hex(orig){
+  const rgb = orig.replace(/\s/g,'').match(/^rgba?\((\d+),(\d+),(\d+)/i);
+
+  return (rgb && rgb.length === 4) ? "#" +
+    ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
+}
+
+//When the submit button is clicked, this resets the table
+$(document).ready(function() {
+  $("#subBtn").click(function() {
+    $("table").find('tr').remove();
+      makeGrid();
+  });
+});
+
+//Method for filling blocks with the color selected
+$(document).ready(function() {
+  $('#pixelCanvas').on('click', 'td', function() {
+    const mCell = $(event.target);
+    const pColor = $('.input-color').val();
+    const cellColor = rgb2hex(mCell.css('background-color'));
+
+    if (cellColor == pColor) { //Checking if the cell has color, if not, it adds color, if it does, it removs the color
+      mCell.css('background-color', '#fff');
+    }else {
+      mCell.css('background-color', pColor);
+    }
+  });
+});
